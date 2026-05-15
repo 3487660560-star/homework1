d30f116deb290eead978233589aa680b52a5ab00
GuoZiyu <3487660560@qq.com>
2026-05-15 23:47:49 +0800

    ex02: add 1Hz blink code with millis( )

---
+++ ex02/ex02_blink_1hz.ino
@@ -0,0 +1,20 @@
+const int ledPin = 2;
+
+unsigned long previousMillis = 0;
+const long interval = 500;
+bool ledState = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+  }
+}
+
